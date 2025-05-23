b8fba6b9e3e8a0dcda054cbba41e24659314e40a
liyunfu1998 <liyunfu1998@qq.com>
2025-05-23 16:53:12 +0800

    feat: tool use demo

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,18 +1,34 @@
-import { createOpenRouter } from '@openrouter/ai-sdk-provider';
-import {streamText} from 'ai'
+import { createOpenRouter } from "@openrouter/ai-sdk-provider";
+import { streamText, tool } from "ai";
+import { z } from "zod";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
-export async function POST(req: Request){
-  const {messages} = await req.json()
+export async function POST(req: Request) {
+  const { messages } = await req.json();
   const openrouter = createOpenRouter({
     apiKey: process.env.OPENROUTER_API_KEY,
-  })
+  });
   const result = streamText({
-    model: openrouter('anthropic/claude-3.5-sonnet-20240620'),
+    model: openrouter("anthropic/claude-3.5-sonnet-20240620"),
     messages,
-  })
+    tools: {
+      weather: tool({
+        description: "Get the weather in a location",
+        parameters: z.object({
+          location: z.string().describe("The location to get the weather for"),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+    },
+  });
 
-  return result.toDataStreamResponse()
+  return result.toDataStreamResponse();
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,38 @@
-"use client"
+"use client";
 
-import {useChat} from '@ai-sdk/react'
+import { useChat } from "@ai-sdk/react";
 
-export default function Chat(){
-  const {messages, input, handleInputChange, handleSubmit} = useChat()
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {
-        messages.map((message)=> (
-          <div className="whitespace-pre-wrap" key={message.id}>
-            {message.role === 'user' ? 'User:' : 'AI'}
-            {message.parts.map((part, i) => {
-              switch(part.type){
-                case 'text':
-                  return <div key={`${message.id}-${i}`}>{part.text}</div>
-              }
-            })}
-          </div>
-        ))
-      }
+      {messages.map((message) => (
+        <div className="whitespace-pre-wrap" key={message.id}>
+          {message.role === "user" ? "User:" : "AI"}
+          {message.parts.map((part, i) => {
+            switch (part.type) {
+              case "text":
+                return <div key={`${message.id}-${i}`}>{part.text}</div>;
+              case "tool-invocation":
+                return (
+                  <pre key={`${message.id}-${i}`}>
+                    {JSON.stringify(part.toolInvocation, null, 2)}
+                  </pre>
+                );
+            }
+          })}
+        </div>
+      ))}
 
       <form onSubmit={handleSubmit}>
-        <input className='fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl' value={input} placeholder='Say something...' onChange={handleInputChange}/>
+        <input
+          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
       </form>
     </div>
-  )
+  );
 }
